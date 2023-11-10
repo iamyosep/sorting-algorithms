e8779b19f6544b87766cb0656fdb071ea2b5ef85
ywrd26 <yousif.wardah@mni.thm.de>
2023-11-10 14:34:25 +0100

    Implement basic file creation and fill with data; Add gitignore for created txt files

--- dataGenerator.js
+++ dataGenerator.js
@@ -1,12 +1,17 @@
-import * as fs from 'fs';
-
+import fs from 'fs';
 
 const AMOUNT_OF_DATA = 1000;
-let data;
 
 const generateInteger = (min = 1, max = 100) => Math.floor(Math.random() * (max - min + 1) + min);
 const generateFloat = (min = 1, max = 100) => Math.random() * (max - min) + min;
 
 const generateData = (amout = AMOUNT_OF_DATA, numberGenerator = generateInteger) => Array.from({ length: AMOUNT_OF_DATA }, () => numberGenerator());
+const generateFile = () => {
+    let data = generateData();
+    fs.writeFile(`${data.length}x${Number.isInteger(data[0])}.txt`, data.toString(), (err) => {
+        if (err) throw err;
+        console.log("File created");
+    });
+}
 
-console.table(generateData())
+generateFile();
