049067cfab1492f132e003e2d65ad2c12373d536
ywrd26 <yousif.wardah@mni.thm.de>
2023-11-13 18:13:12 +0100

    Implement insertion sort + Improve CLI

--- runner.js
+++ runner.js
@@ -1,15 +1,55 @@
 import { performance } from 'perf_hooks';
 import { getData } from './dataReader.js';
 import { bubbleSort, bubbleSort2 } from './bubble-sort.js';
+import { insertionSort } from './insertion-sort.js';
 
-const data = getData();
+const REQUIRED_ARGS = 5;
+const ARGS = process.argv;
+
+if (ARGS.length < REQUIRED_ARGS || ARGS.length > REQUIRED_ARGS) {
+    console.log(`Please use the following format:
+$ node runner.js <file> <sorting>`);
+    process.exit(1);
+}
+
+const sortingFunctions = {
+    bubble: {
+        name: 'Bubble Sort',
+        function: bubbleSort,
+    },
+    bubble2: {
+        name: 'Alt. Bubble Sort',
+        function: bubbleSort2,
+    },
+    insertion: {
+        name: 'Insertion Sort',
+        function: insertionSort,
+    },
+}
+
+const information = {
+    file: ARGS[2],
+    sorting: sortingFunctions[ARGS[3]],
+    repetition: ARGS[4],
+}
+
+const data = getData(information.file);
+
+/* 
+    Log the information about the file and sorting algorithm
+    that will be used.
+    If no arguments are passed, the default values will be used.
+*/
+
 console.log(
     "\x1b[33m%s\x1b[0m", 
 `
-Bubblesort to be performed on ${data.length} numbers.
-The sorting will be performed 100 times and the average time will be calculated.
+${information.sorting.name} to be performed on ${information.file}.
+The sorting will be performed ${information.repetition} times.
+This may take a while...
 `
 );
+
 
 const runFunction = (data, sortingFunction, repetition) => {
     const start = performance.now();
@@ -21,17 +61,9 @@ const runFunction = (data, sortingFunction, repetition) => {
     return end - start;
 }
 
-let time = runFunction(data, bubbleSort, 100);
-let time2 = runFunction(data, bubbleSort2, 100);
 
-console.log("\x1b[31m%s\x1b[0m", "Bubblesort:");
-console.log(`Total: ${time}ms
-Avg: ${time / 100}ms`);
+let time = runFunction(data, information.sorting.function, information.repetition);
 
-console.log("");
-
-console.log("\x1b[31m%s\x1b[0m", "Bubblesort2:");
-console.log(`Total: ${time2}ms
-Avg: ${time2 / 100}ms`);
-
-console.log("");
+console.log("\x1b[31m%s\x1b[0m", `${information.sorting.name} - ${information.repetition} repetitions`);
+console.log(`Total: ${time} ms
+Avg: ${time / information.repetition} ms`);
